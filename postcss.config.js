2503942fcb8933b9171475d30074928566295d57
Smol Codes <rbrittbusler92@gmail.com>
2021-01-18 19:56:00 -0500

    added for stuff

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,7 @@ module.exports = {
     require("postcss-import"),
     require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer"),
+    require('postcss-preset-env')({ stage: 1 }),
+    require('postcss-clean'),
   ],
 };
