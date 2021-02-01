5bdac1b4a6c83b3d977b9e3b407613a29d629563
Smol Codes <rbrittbusler92@gmail.com>
2021-01-31 22:05:53 -0500

    switched templates from html to nkk

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,5 +90,5 @@ module.exports = {
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
-  purge: ["./src/**/*.js", "./src/**/*.html", "./src/**/*.svg"],
+  purge: ["./src/**/*.js", "./src/**/*.njk", "./src/**/*.svg"],
 };
