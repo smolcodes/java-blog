c4b2075b8a46170b522d300c2e558585b741418d
Smol Codes <rbrittbusler92@gmail.com>
2021-01-31 22:04:44 -0500

    added markdown highlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const rssPlugin = require('@11ty/eleventy-plugin-rss');
+const htmlmin = require("html-minifier");
 const Image = require("@11ty/eleventy-img");
 const sharp = require("sharp");
-
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
@@ -11,7 +11,52 @@ const w3DateFilter = require('./src/filters/w3-date-filter.js');
 // Create a helpful production flag
 const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
 
-module.exports = function (eleventyConfig) {
+module.exports = (eleventyConfig) => {
+  //html minifier
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if (outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+    return content;
+  });
+
+  /// Markdown Component from Markdown-it
+  const md = require('markdown-it')();
+  md.use(require('markdown-it-footnote').default);
+  md.use(require('markdown-it-container').default);
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options)
+    .use('url_new_win', 'link_open', function (tokens, idx) {
+      const [attrName, href] = tokens[idx].attrs.find(attr => attr[0] === 'href');
+      if (href && (!href.includes('franknoirot.co') && !href.startsWith('/') && !href.startsWith('#'))) {
+        tokens[idx].attrPush(['target', '_blank']);
+        tokens[idx].attrPush(['rel', 'noopener noreferrer']);
+      }
+    })
+
+    .use(markdownItFootnote)
+    .use(markdownItContainer, 'callout')
+    .use(markdownItContainer, 'callout-blue')
+    .use(markdownItContainer, 'callout-pink')
+    .use(markdownItContainer, 'callout-green')
+    .use(markdownItContainer, 'warning')
+
+  );
+
+
   //shortcodes
   eleventyConfig.addNunjucksAsyncShortcode("Image", async (src, alt) => {
     if (!alt) {
@@ -43,7 +88,7 @@ module.exports = function (eleventyConfig) {
           (_acc, curr) => `${_acc} ${curr.srcset} ,`,
           ""
         ),
-      }),
+      })
     );
 
     const source = `<source type="image/webp" data-srcset="${srcset["webp"]}" >`;
@@ -71,7 +116,6 @@ module.exports = function (eleventyConfig) {
     excerpt_separator: "<!-- excerpt -->",
   });
   // Add filters
-
   eleventyConfig.addFilter('dateFilter', dateFilter);
   eleventyConfig.addFilter('w3DateFilter', w3DateFilter);
 
@@ -79,18 +123,18 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(rssPlugin);
   eleventyConfig.addPlugin(pluginNavigation);
 
-    // Returns books items, sorted by display order
-    eleventyConfig.addCollection('books', collection => {
-      return sortByDisplayOrder(
-        collection.getFilteredByGlob('./src/books/*.md'));
-    });
+  // Returns books items, sorted by display order
+  eleventyConfig.addCollection('books', collection => {
+    return sortByDisplayOrder(
+      collection.getFilteredByGlob('./src/books/*.md'));
+  });
 
-    
-    // Returns books items, sorted by display order
-    eleventyConfig.addCollection('javanotes', collection => {
-      return sortByDisplayOrder(
-        collection.getFilteredByGlob('./src/javanotes/*.md'));
-    });
+
+  // Returns books items, sorted by display order
+  eleventyConfig.addCollection('javanotes', collection => {
+    return sortByDisplayOrder(
+      collection.getFilteredByGlob('./src/javanotes/*.md'));
+  });
 
   // Returns a collection of blog posts in reverse date order
   eleventyConfig.addCollection('blog', collection => {
@@ -102,11 +146,11 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/css");
-   eleventyConfig.addPassthroughCopy('src/admin');
+  eleventyConfig.addPassthroughCopy('src/admin');
 
   eleventyConfig.setTemplateFormats(["jpg", "png", "webp", "md", "njk", "html"]);
   return {
-    markdownTemplateEngine: 'njk',
+    markdownTemplateEngine: 'liquid',
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     dir: {
